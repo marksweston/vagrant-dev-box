286967dda90bc97d1a46aa5fd94ddd7b1e7cf9ef
Mark Weston <mark@Marks-MacBook-Pro.local>
2018-04-11 20:06:18 +0100

    Okay, so postgres root accounts with no passwords are frowned upon
    So instead we create a postgres superuser account called rails, with password rails
    And set a DATABASE_URL and TEST_DATABASE_URL env variable in .bashrc to point to
    the local database using that account.
    
    This works transparently for the development environment in Rails, and requres a one-liner
    for the test environment;
      url:  <%= ENV.fetch("TEST_DATABASE_URL", "") %>

--- provision/install-databases.sh
+++ provision/install-databases.sh
@@ -11,10 +11,15 @@ if ! dpkg -s postgresql; then
   sudo apt-get install -y postgresql libpq-dev postgresql-contrib
 
   echo "Setting up user"
-  sudo -u postgres bash -c "psql -c \"CREATE USER vagrant WITH PASSWORD 'vagrant';\""
-  sudo -u postgres bash -c "psql -c \"ALTER USER vagrant WITH SUPERUSER;\""
-  sudo -u postgres bash -c "psql -c \"CREATE USER root;\""
-  sudo -u postgres bash -c "psql -c \"ALTER USER root WITH SUPERUSER;\""
+  sudo -u postgres createuser -s rails
+  sudo -u postgres bash -c "psql -c \"ALTER USER rails WITH PASSWORD 'rails';\""
+  sudo -u postgres createdb rails
+  # sudo -u postgres bash -c "psql -c \"CREATE USER vagrant WITH PASSWORD 'vagrant';\""
+  # sudo -u postgres bash -c "psql -c \"ALTER USER vagrant WITH SUPERUSER;\""
+  # sudo -u postgres bash -c "psql -c \"CREATE USER root;\""
+  # sudo -u postgres bash -c "psql -c \"ALTER USER root WITH SUPERUSER;\""
+  echo "export DATABASE_URL=postgresql://rails:rails@localhost:5432" >> /home/vagrant/.bashrc
+  echo "export TEST_DATABASE_URL=postgresql://rails:rails@localhost:5432" >> /home/vagrant/.bashrc
 
   echo "Starting Postgres server"
   sudo service postgresql start
